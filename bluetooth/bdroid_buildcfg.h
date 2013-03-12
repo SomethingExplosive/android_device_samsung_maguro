64ddb6e5f3c4d4e62f391a7defbdcda061ca7a97
something15525 <something15525@gmail.com>
2013-03-12 15:16:18 -0700

    maguro: fix default bluetooth name

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2012 The Android Open Source Project
+ * Copyright (C) 2013 The SomethingExplosive Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,6 +18,6 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME   "Galaxy Nexus 2"
+#define BTM_DEF_LOCAL_NAME   "Galaxy Nexus"
 
 #endif
